be201c758f821613933eac91e857e9d143d4af2f
fkwsurm <guswl543210@gmail.com>
2021-07-16 19:25:19 +0900

    백업

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 const dotenv = require('dotenv');
 dotenv.config();
 const { PORT  } = process.env;
+app.use(require("cors")());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
 const http_server = require('http')
 .createServer(app)
@@ -26,11 +31,38 @@ db.sequelize
     console.log('db' + err);
 });
 
-
-app.post('/create', (req, res) => {
-  try{
-
-  }catch(err){
-    console.log(err);
+const schema = buildSchema(`
+  type Query {
+    hello: String,
+    fuckyou : String
   }
-})
+`);
+ 
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  fuckyou: () => {
+    return 'fuck~ you~ go~ away~';
+  }
+};
+ 
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+//using in restapi
+app.get("/get_graphql", async (req, res) => {
+  try {
+      let graphql_api = req.query.api;
+      let response = await graphql(schema, `{ ${graphql_api} }`, root);
+      res.status(200).send(response);
+  } catch (error) {
+      console.log(error);
+  }
+});
+
+console.log('Running a GraphQL API server at http://localhost:8080/graphql');
